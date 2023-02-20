f078344c53d729468eb9573dbb1c6226b6f58ee4
Tanishka <makodetanishka@gmail.com>
2023-02-20 23:25:45 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  // content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Comfotaa: ["Comfortaa", "cursive"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
